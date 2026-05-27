66fc7ae94dc11f19257e90bb76ad090f79eada15
LucasJ574 <diguglielmolucas@gmail.com>
2026-05-27 15:10:38 +1000

    Added instructions

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# This file is in /home/nvidia/
+# This file is located in /home/nvidia/
 
 # This run script runs the following commands to immediately start the 
 # qbot platform launch code without the need to type all of these files
 
+# To run this script in the Qbot terminal type "./run.sh"
 
 cd ~/ros2
 source /opt/ros/humble/setup.bash
