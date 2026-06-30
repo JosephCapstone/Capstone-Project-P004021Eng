87543f7e8539e842420665074ebd89cbe0e52a2a
LucasJ574 <diguglielmolucas@gmail.com>
2026-06-30 10:35:52 +1000

    Removed local colcon build files for qbot_platform  & made a gitignore file to ensure they are not uploaded to the github

--- Team_01/build/qbot_platform/CMakeFiles/qbot_platform_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/qbot_platform_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/qbot_platform_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
