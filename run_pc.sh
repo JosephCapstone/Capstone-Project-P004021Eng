27c3228aff1d2b8712cb5c1a2f46a515ea6209eb
LucasJ574 <diguglielmolucas@gmail.com>
2026-07-07 12:47:24 +1000

    renamed and modified the run_pc file

---
+++ run_pc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This file is located on the PC in /home/ernie/
+
+# This run script runs the following commands to 
+# 1. List's all topics broadcasted from the QBot to confirm connection
+# 2. Runs the RGB camera viewer
+
+# To run this script in the VSCode WSL Ubuntu 22.04 bash terminal type ./run_pc.sh
+
+## THINGS TO NOTE ##
+# - THE ONLY WAY THIS CODE WORKS IS IF THE WINDOWS FIREWALL ON THE PRIVATE NETWORK IS TURNED OFF 
+#   (A fix is to add a rule to allow data from the qbot to be allowed through the firewall)
+
+source /opt/ros/humble/setup.bash
+ros2 daemon start
+ros2 topic list
+
+ros2 run rqt_image_view rqt_image_view
