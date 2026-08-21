b30593f450ec856f85eac159de32c467ee443aef
LucasJ574 <diguglielmolucas@gmail.com>
2026-08-21 16:04:10 +1000

    updated run_record and run_qbot to work with the DeltaUI

--- run_qbot.sh
+++ run_qbot.sh
@@ -6,22 +6,24 @@
 # 1. Builds the Ros2 workspace and compiles any update files
 # 2. Runs the QBot launch file
 # 3. Runs the Ouster OS0 LiDAR launch file
+# 4. Launches the Foxglove bridge launch file
 
-# To run this script, type "./run_qbot.sh" in the QBot terminal.
-# To end both processes enter "ctrl+c"
+# To run this script, use the ALL NEW DeltaUI!!!!!
 
 cd ~/ros2
 source /opt/ros/humble/setup.bash
 colcon build
 source install/setup.bash
 
-
-ros2 launch qbot_platform qbot_platform_manual_drive_launch.py &
+setsid ros2 launch qbot_platform qbot_platform_manual_drive_launch.py &
 PID1=$!
 
-ros2 launch ouster_ros sensor.launch.xml sensor_hostname:=os-992123000057.local proc_mask:="RAW|PCL|IMU|SCAN|TLM" &
+setsid ros2 launch ouster_ros sensor.launch.xml sensor_hostname:=os-992123000057.local proc_mask:="RAW|PCL|IMU|SCAN|TLM" &
 PID2=$!
 
-trap 'kill -INT $PID1 $PID2' SIGINT
+setsid ros2 launch foxglove_bridge foxglove_bridge_launch.xml port:=8765 &
+PID3=$!
+
+trap 'kill -INT -- -$PID1 -$PID2 -$PID3' SIGINT SIGTERM
 
 wait
--- run_record.sh
+++ run_record.sh
@@ -7,23 +7,12 @@
 # 2. Start the ROS Bag recording
 # 3. Allows for naming of file and manual starting/ending of the recording
 
-# To run this script, type "./run_record.sh" in the QBot terminal.
-# Type the number (#) of the current test, the name of the ros bag will be "test_#"
-# Press ENTER to start the recording
-# To end the recording press "q"
+# To run this script, use the ALL NEW DeltaUI!!!!!
 
 cd ~/ros2/bags
 source /opt/ros/humble/setup.bash
-source ~/ros2/install/setup.bash
+source ~/ros2/install/setup.bash #might not be needed
 
-read -p 'Recording Number ' BAG_NUM
-read -p 'Press ENTER to start recording...'
+BAG_NUM=$1
 
-ros2 bag record -o "test_$BAG_NUM" /ouster/lidar_packets /ouster/imu_packets /ouster/metadata /tf /tf_static /qbot_imu &
-BAG_PID=$!
-
-read -n 1 -p "Press q to stop recording: " KEY
-
-if [ "$KEY" = "q" ]; then
-    kill $BAG_PID
-fi
+ros2 bag record -o "test_$BAG_NUM" /ouster/lidar_packets /ouster/imu_packets /ouster/metadata /tf /tf_static /qbot_imu /ouster/scan &
